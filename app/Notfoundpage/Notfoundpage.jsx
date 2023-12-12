56b80f72fe4fdf430f0e71fd576d88284dbc4d3d
Chernik Roman <zebrentv@gmail.com>
2023-12-12 22:11:52 +0600

    del useless page

--- app/Notfoundpage/Notfoundpage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import styles from './Notfoundpage.module.scss';
-
-// вставить грустную уточку об отсутсвии страницы
-export const Notfoundpage = 
-    () => <h1 className={styles.title}>Pege Notfoundpage</h1>;
