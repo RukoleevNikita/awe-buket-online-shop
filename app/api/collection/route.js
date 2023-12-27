b3e0d526625517991fbf189e56f8be59bafda8bc
Roman Chernik <zebrentv@gmail.com>
2023-12-27 22:42:33 +0600

    Получение всех товаров

---
+++ app/api/collection/route.js
@@ -0,0 +1,12 @@
+import { NextResponse } from 'next/server';
+import connectMongo from '@/lib/mongo/mongodb';
+import CardProduct from '@/models/CardProduct';
+export async function GET(request, {params}) {
+  try {
+    await connectMongo();
+    const product = await CardProduct.find();
+    return new NextResponse(JSON.stringify(product), { status: 200 });
+  } catch (e) {
+    console.log(e);
+  }
+}
